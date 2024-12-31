bc6c1668b6de15321f2a6c350a6fced4cf469027
woomin422@gmail.com <woomin422@gmail.com>
2024-12-31 18:30:43 +0900

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+const express =  require("express")
+const app = express()
+const path = require("path")
+const fs = require('fs');
+
+let db
+const filePath = path.join(__dirname, 'data.json');
+console.log(filePath)
+
+fs.readFile(filePath, 'utf8', (err, data)=>{
+    if(err){
+        console.error(err);
+        return;
+    }
+    db = JSON.parse(data)
+    console.log(db.soccer.name)
+})
+  
+app.set('view engine', 'ejs')
+app.set(__dirname, 'views' )
+
+app.get('/r/:subreddit', (req,res)=>{
+    console.log(req)
+})
+
+app.listen(3000, console.log("http://localhost:3000"))
