5a1551f95a947436221e317850b4369146aca130
woominKo <woomin42@naver.com>
2025-01-01 11:15:59 +0900

    dd

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express =  require("express")
 const app = express()
 const path = require("path")
 const fs = require('fs');
+app.set('view engine', 'ejs')
+app.set(__dirname, 'views' )
 
 let db
 const filePath = path.join(__dirname, 'data.json');
@@ -16,12 +18,15 @@ fs.readFile(filePath, 'utf8', (err, data)=>{
     console.log(db.soccer.name)
 })
   
-app.set('view engine', 'ejs')
-app.set(__dirname, 'views' )
+
 
 app.get('/r/:subreddit', (req,res)=>{
     const subreddit = req.params.subreddit
-    res.render('index',{db:db} )
+    // res.render('index',{db:db} )
+    if(subreddit == "soccer"){
+        res.render('soccer',{db:db})
+
+    }
 })
 
 app.listen(3000, console.log("http://localhost:3000"))
